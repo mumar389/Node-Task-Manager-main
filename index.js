ee9d65084ed8b675b5a8603da742c93a0b004c60
Md Umar <mohammadumar389@gmail.com>
2023-03-08 15:50:16 +0530

    App

--- index.js
+++ index.js
@@ -1,27 +1,28 @@
-const express = require('express')
+const express = require("express");
 // require('dotenv').config();
 const index = express();
-const tasks = require('./routes/tasks')
-const connectDB = require('./db/connect')
-const dotenv = require("dotenv")
-dotenv.config()
+const tasks = require("./routes/tasks");
+const connectDB = require("./db/connect");
+const dotenv = require("dotenv");
+dotenv.config();
 
-index.use(express.static('./public'))
-console.log('Task Manager App')
+index.use(express.static("./public"));
+console.log("Task Manager App");
 // app.use(express.urlencoded({extended: false}))
-index.use(express.json())
-index.use('/',require('./routes/tasks'))
-// index.use('/api/v1/tasks', tasks)
+index.use(express.json());
+// index.use('/',require('./routes/tasks'))
+index.use("/api/v1/tasks", tasks);
 
-const port = process.env.PORT || 3000
-const start = async()=>{
-    await connectDB(process.env.MONGO_URI)
-        .then(() => {console.log('CONNECTED TO DB....')
-        })
-        .catch(error => console.log(error))
-}
-start().then(()=>{
-    index.listen(port, () => {
-        console.log('Server is listening at port 3000')
+const port = process.env.PORT || 3000;
+const start = async () => {
+  await connectDB(process.env.MONGO_URI)
+    .then(() => {
+      console.log("CONNECTED TO DB....");
     })
-})
+    .catch((error) => console.log(error));
+};
+start().then(() => {
+  index.listen(port, () => {
+    console.log("Server is listening at port 3000");
+  });
+});
